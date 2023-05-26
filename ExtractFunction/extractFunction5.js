efce0223c5f036a5a551e7c204be4891dc81a395
PavelM <myagkiypavel@gmail.com>
2023-05-26 20:46:31 +0300

    feat: add extract function 5 example

---
+++ ExtractFunction/extractFunction5.js
@@ -0,0 +1,28 @@
+function generatePasswordTest(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+  return password;
+}
+
+// Refactor Extruct Function
+
+function generatePasswordTest(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += getRendomCharacter(characters);
+  }
+
+  return password;
+}
+
+function getRendomCharacter(characters) {
+  const randomIndex = Math.floor(Math.random() * characters.length);
+  return characters.charAt(randomIndex);
+}
